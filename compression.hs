272c5efddae14c9b3aaa6fb3a3e1590ef2ef9b24
HammadSiddiqui <ronytop12@yahoo.com>
2016-05-08 18:47:17 +0500

    Tab warnings fix

--- compression.hs
+++ compression.hs
@@ -11,27 +11,31 @@ import Data.List (sort)
 import Data.List.Split
 import System.IO   
 import Data.Char
+--------------------- Custom Types -------------------------------
+
 type Dict = [(Int , String, Int)] -- Custom type to store dicitonary
 type BWT = [String]  -- Custom type for BWT matrix
    
-
+--------------- Main Functions to run on terminal ----------------
+-- Compression with BWT Transform
 compressionBWT fname outname = do  
-		    contents <- readFile fname
-		    writeFile outname (mergeOut (compress ([head (genBWTmatrix contents)]) (tail (genBWTmatrix contents)) []))
+            contents <- readFile fname
+            writeFile outname (mergeOut (compress ([head (genBWTmatrix contents)]) (tail (genBWTmatrix contents)) []))
 		 
-
+-- Compression without BWT Transform
 compressionNoBWT fname outname = do  
-		    contents <- readFile fname
-		    writeFile outname (mergeOut (compress ([head  contents]) (tail contents) []))
+            contents <- readFile fname
+            writeFile outname (mergeOut (compress ([head  contents]) (tail contents) []))
 		  
-
+-- Decompression with BWT Transform
 decompressionBWT fname outname = do  
-			    compressedStr <- readFile fname
-			    writeFile outname (inverseBWT (decompress (splitOn " " compressedStr) []))
+            compressedStr <- readFile fname
+            writeFile outname (inverseBWT (decompress (splitOn " " compressedStr) []))
 
+-- decompression without BWT Transform
 decompressionNoBWT fname outname = do  
-			    compressedStr <- readFile fname
-			    writeFile outname (decompress (splitOn " " compressedStr) [])
+            compressedStr <- readFile fname
+            writeFile outname (decompress (splitOn " " compressedStr) [])
 
    
 ----------------------Compression-------------------------------
@@ -45,16 +49,16 @@ thd (_, _, a) = a
 --Takes a string(key) and a dictionary and returns its value.
 findStr :: String -> Dict -> Int
 findStr str lst
-	|lst == [] = -1
-	|sd (head lst) == str = thd (head lst)
-	|otherwise = findStr str (tail lst)
+    |lst == [] = -1
+    |sd (head lst) == str = thd (head lst)
+    |otherwise = findStr str (tail lst)
 
 
 --Takes a string(key) and a dictionary and returns its value. The value is its Ascii code if its a character
 value :: String -> Dict-> Int
 value str d
-	|length str == 1 = ord (head str)
-	|otherwise = findStr str d
+    |length str == 1 = ord (head str)
+    |otherwise = findStr str d
 
 --adds (output ,key, value) to the dictionary. Called when a new string is added to the dictioanry.  
 add :: String -> String -> Dict -> Dict
@@ -66,15 +70,15 @@ maxIndex d = 255 + (length d)
 --performs the Lempel Ziv Welch compression algorithm
 compress :: String -> String -> Dict -> Dict
 compress current str d
-	|str == "" =  d
-	|(findStr (current ++ [str!!0]) d) /= -1 = compress (current ++ [str !! 0]) (tail str) d --(current + next) already present in d. current = current + next
-	|otherwise = compress ([str !! 0]) (tail str) (add current (current ++ [str !! 0]) d) -- add (current + next) to d. save the value of current for output 
+    |str == "" =  d
+    |(findStr (current ++ [str!!0]) d) /= -1 = compress (current ++ [str !! 0]) (tail str) d --(current + next) already present in d. current = current + next
+    |otherwise = compress ([str !! 0]) (tail str) (add current (current ++ [str !! 0]) d) -- add (current + next) to d. save the value of current for output 
 
 --extract and merge all the ouput values from d, separated by spaces
 mergeOut :: Dict -> String
 mergeOut lst
-	|lst == [] = ""
-	|otherwise = show (ft (head lst)) ++ " " ++ mergeOut (tail lst) 
+    |lst == [] = ""
+    |otherwise = show (ft (head lst)) ++ " " ++ mergeOut (tail lst) 
 
 
 ------------------ Decompress -----------------
@@ -92,32 +96,30 @@ decompress :: [String] -> Dict -> String
 decompress [] d = "" 
 decompress [""] d = "" 
 decompress lst@(x:xs) d = if (read x :: Int) <= 255  then [chr (read x :: Int)] ++ (decompress xs (add2 [chr (read (x) :: Int)] [chr (read (head xs) :: Int)] d))
---	| (read x :: Int) > 255 = [findInt x d]
---	| x == "" = []
-	else (findInt (read x :: Int) (add2 x (head xs) d)) ++ (decompress xs (add2 [chr (read (x) :: Int)] [chr (read (head xs) :: Int)] d))
---if findStr (head substr) d
+    else (findInt (read x :: Int) (add2 x (head xs) d)) ++ (decompress xs (add2 [chr (read (x) :: Int)] [chr (read (head xs) :: Int)] d))
+
 
 -- Dictionary addition for decompress
 add2 :: String -> String  -> Dict -> Dict
 add2 str1 str2 d = d ++ [(0, (str1++str2), (maxIndex d) + 1)] 
 
 
--- Finds the string corrosponding to a value greater then 255
+-- Finds the string corrosponding to a value greater then 255 in the dictionary
 findInt :: Int -> Dict -> String
 findInt i [] = [chr i]
 findInt i d@(x:xs) 
-	| i <= 255 = [chr i]
+    | i <= 255 = [chr i]
     | thd x == i = sd x
-	| thd x /= i = findInt i xs
-	| otherwise = ""
+    | thd x /= i = findInt i xs
+    | otherwise = ""
 
 
 --------------------- BWT -----------------------
 --converts a string of the form "cats" to ["c" , "a", "t" , "s"]	   
 listChar :: String -> [String]
 listChar str
-	|length str == 1 = [str] 
-	|otherwise = [[head str]] ++ listChar (tail str) 
+    |length str == 1 = [str] 
+    |otherwise = [[head str]] ++ listChar (tail str) 
 
 -- takes a string and performs inverse BWT tranform on it
 inverseBWT :: String -> String
@@ -130,14 +132,14 @@ inverseBWT2 lst = inverseBWT1 lst lst
 --helper function for inverseBWT
 inverseBWT1:: [String] -> [String] -> String
 inverseBWT1 lst1 lst
- | length lst1 == length (lst !! 0)= init (originalString lst)
- | otherwise = inverseBWT1 lst1 (concatList lst1 (sort lst))
+    | length lst1 == length (lst !! 0)= init (originalString lst)
+    | otherwise = inverseBWT1 lst1 (concatList lst1 (sort lst))
 
 --returns the string that ends with '$'
 originalString :: [String] -> String
 originalString lst 
-	|last (lst !! 0) == '$' = lst !! 0
-	|otherwise = originalString (tail lst)
+    |last (lst !! 0) == '$' = lst !! 0
+    |otherwise = originalString (tail lst)
 
 -- if list1  = ["c" , "a", "t" , "s"] , list2 =  ["b" , "a", "t" , "s"] : concatList =  ["cb" , "aa", "tt" , "ss"]
 -- Run on same length lists
@@ -158,7 +160,7 @@ rotateBWT a = if (head l) == '$' then a
 
 
 ----------------------------- Dr. Waqar's Strategy for BWT ------------------
-someString = ["Anas"]
+someString = ["Banana"]
 genBWTmatrix2 = (transpose (sort(transpose (rotateBWT2 [(head someString) ++ "$"])))) !! 1
 
 rotateBWT2 :: BWT -> BWT
